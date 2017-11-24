ed5203e85b04cfe4de42a7073f725d6a986c5eff
Steve Zaske <github@zaske.com>
2017-11-23 17:40:59 -0800

    removing cloud functions

--- functions/index.js
+++ functions/index.js
@@ -1,31 +1,8 @@
 const functions = require('firebase-functions');
-var Filter = require('bad-words');
 
-exports.sanitizeName = functions.database
-  .ref('/games/{gameId}/player_list/{playerId}')
-  .onWrite(event => {
-    const player = event.data.val();
-
-    // have we checked this name before?
-    // Coded added because without it we'd create an infinite loop
-    if (player.sanitized) {
-      return
-    }
-
-    // Nope. let's check it out
-    filter = new Filter();
-    if (filter.isProfane(player.name)) {
-      badword = player.name;
-      player.name = filter.clean(player.name);
-      console.log("Filtered name in game=" +
-        event.params.gameId +
-        ", player name:" +
-        badword +
-        " changed to:" +
-        player.name)
-    }
-
-    player.sanitized = true;
-    // This returns a promise
-    return event.data.ref.set(player)
-  })
+// // Create and Deploy Your First Cloud Functions
+// // https://firebase.google.com/docs/functions/write-firebase-functions
+//
+// exports.helloWorld = functions.https.onRequest((request, response) => {
+//  response.send("Hello from Firebase!");
+// });
